3a13acd2ac7432a947f6c2bb5d88a33c669e8f3f
Michael-Cassidy-88 <mjcdyjr10@gmail.com>
2022-07-16 01:04:20 -0500

    Three Tier Architecture

---
+++ AWS-Three-Tier_Architecture/modules/three-tier-deployment/compute/outputs.tf
@@ -0,0 +1,9 @@
+# --- compute/outputs.tf ---
+
+output "app_asg" {
+  value = aws_autoscaling_group.three_tier_app
+}
+
+output "app_backend_asg" {
+  value = aws_autoscaling_group.three_tier_backend
+}
---
+++ AWS-Three-Tier_Architecture/terraform/dev/us-east-1/t2.micro/three-tier-deployment/terraform.tfvars
@@ -0,0 +1,4 @@
+access_ip  = "0.0.0.0/0"
+db_name    = "threetierdb"
+dbuser     = "admin"
+dbpassword = "xxxxxxxxxxxxx"
